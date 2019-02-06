4fef39451db1db570e9f1d7df532c0132dd14fe5
SMuyesser <smuyesser@gmail.com>
2019-02-05 18:38:47 -0800

    all info added without styling

--- js.js
+++ js.js
@@ -1,58 +1,56 @@
 // Hides landing animation after it leaves screen
 window.onload = function() {
-	$("img#intro-img").addClass("loaded");
-	setTimeout(() => {
-	  $("div#intro-animation").addClass("hidden")
-	}, 3000);
+    $("img#intro-img").addClass("loaded");
+    setTimeout(() => {
+        $("div#intro-animation").addClass("hidden")
+    }, 3000);
 };
 
 
 // About Link
 $("#about-link").on("click", function() {
-	if (!$(".portfolio").hasClass("hidden")) {
-		$(".portfolio").addClass("hidden");
-	};	
-	if (!$(".contact").hasClass("hidden")) {
-		$(".contact").addClass("hidden");
-	};
+    if (!$(".portfolio").hasClass("hidden")) {
+        $(".portfolio").addClass("hidden");
+    };
+    if (!$(".contact").hasClass("hidden")) {
+        $(".contact").addClass("hidden");
+    };
     $(".about").removeClass("hidden");
 });
 
 // Portfolio Link
 $("#portfolio-link").on("click", function() {
-	if (!$(".about").hasClass("hidden")) {
-		$(".about").addClass("hidden");
-	};	
-	if (!$(".contact").hasClass("hidden")) {
-		$(".contact").addClass("hidden");
-	};
+    if (!$(".about").hasClass("hidden")) {
+        $(".about").addClass("hidden");
+    };
+    if (!$(".contact").hasClass("hidden")) {
+        $(".contact").addClass("hidden");
+    };
     $(".portfolio").removeClass("hidden");
 });
 
 //Contact Link
 $("#contact-link").on("click", function() {
-	if (!$(".about").hasClass("hidden")) {
-		$(".about").addClass("hidden");
-	};	
-	if (!$(".portfolio").hasClass("hidden")) {
-		$(".portfolio").addClass("hidden");
-	};
+    if (!$(".about").hasClass("hidden")) {
+        $(".about").addClass("hidden");
+    };
+    if (!$(".portfolio").hasClass("hidden")) {
+        $(".portfolio").addClass("hidden");
+    };
     $(".contact").removeClass("hidden");
 });
 
 $("#toggle").click(function() {
-	$(this).toggleClass('on');
-	$('#sidebar').toggleClass('active');
+    $(this).toggleClass('on');
+    $('#sidebar').toggleClass('active');
 })
 
 function openSlideMenu() {
-	$('#toggle').attr('onclick', 'closeSlideMenu()');
-	document.getElementById('sidebar').style.width= '200px';
-	document.getElementById('main').style.marginRight= '200px';
+    $('#toggle').attr('onclick', 'closeSlideMenu()');
+    document.getElementById('sidebar').style.width = '200px';
 }
 
 function closeSlideMenu() {
-	$('#toggle').attr('onclick', 'openSlideMenu()');
-	document.getElementById('sidebar').style.width= '0px';
-	document.getElementById('main').style.marginRight= '0px';
+    $('#toggle').attr('onclick', 'openSlideMenu()');
+    document.getElementById('sidebar').style.width = '0px';
 }
